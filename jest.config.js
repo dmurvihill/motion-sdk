893e32ee534883970b70322214ce1448a38eb5ab
Dolan Murvihill <dolan.murvihill@downstream.com>
2024-10-24 08:44:56 -0700

    Add coverage enforcement

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+// @ts-check
+
+/** @type {import('jest').Config} */
+export default {
+  transform: {
+    "^.+\\.ts?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
