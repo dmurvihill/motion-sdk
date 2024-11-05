51e81a96c4ffe2a69572f404107a52e9b9eecbc2
Dolan Murvihill <dolan.murvihill@downstream.com>
2024-11-05 02:20:18 -0800

    Move motionClientFromEnvironment to utils
    Well, that didn't last long, we already did something we said we weren't
    going to do.
    
    Turns out, we have multiple files that do risky tests. It's safer to
    have them share as much synchronization logic as possible, to avoid
    things like keys getting out of sync.

--- __tests__/util/fixtures.ts
+++ __tests__/util/fixtures.ts
@@ -1,10 +1,10 @@
-import { RedisOptions } from "ioredis";
+import { Redis, RedisOptions } from "ioredis";
 import {
   Motion,
   MotionOptions,
   recommendedRateLimits,
 } from "../../src/index.js";
-import { RateLimiterMemory } from "rate-limiter-flexible";
+import { RateLimiterMemory, RateLimiterRedis } from "rate-limiter-flexible";
 import { motionMockBaseUrl } from "../../src/constant.js";
 
 export function redisConfigFromEnvironment(keyPrefix?: string): RedisOptions {
@@ -36,4 +36,56 @@ export function inMemoryTestClient(opts: Partial<MotionOptions> = {}) {
       opts,
     ),
   );
+}
+
+/* WARNING. Make sure you are using the same Redis server and limiter
+keys _everywhere_ you are calling the real Motion API. */
+export async function motionClientFromEnvironment(
+  redis: Redis,
+  baseUrl?: string,
+): Promise<Motion> {
+  baseUrl = baseUrl ?? process.env.MOTION_TEST_TARGET ?? motionMockBaseUrl;
+  const keyPrefix = `limiter:${baseUrl.replace(/:/g, "")}`;
+  const requestLimiter = new RateLimiterRedis({
+    storeClient: redis,
+    keyPrefix,
+    ...recommendedRateLimits.requests,
+  });
+  const overrunLimiter = new RateLimiterRedis({
+    storeClient: redis,
+    keyPrefix,
+    ...recommendedRateLimits.overruns,
+  });
+  const apiKey = process.env.MOTION_API_KEY ?? "";
+  const userId = process.env.MOTION_USER_ID ?? "";
+  const motion = new Motion({
+    baseUrl,
+    apiKey,
+    userId,
+    requestLimiter,
+    overrunLimiter,
+  });
+  if (apiKey === "") {
+    motion.close(
+      "API key not set; set the MOTION_API_KEY environment variable",
+    );
+  } else if (userId === "") {
+    motion.close(
+      "User ID not set; set the MOTION_USER_ID environment variable",
+    );
+  } else {
+    const limiterCheck = overrunLimiter
+      .get(motion.overrunLimiterKey)
+      .catch((e: unknown) => {
+        motion.close("Unable to get overrun limiter status; refusing to start");
+        console.error(e);
+        throw e;
+      });
+    const overrunsToday = (await limiterCheck)?.consumedPoints ?? 0;
+    if (overrunsToday > 0) {
+      motion.close("Already had an overrun today; refusing to start");
+    }
+  }
+
+  return motion;
 }
